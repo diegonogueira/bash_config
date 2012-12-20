c2d58027cb3fbe47952f65c428cd5cff1b38cfcb
Jhonathas Matos <jhonathas@gmail.com>
2012-12-20 15:26:14 -0200

    Changed command from alias

--- scripts/railroad.sh
+++ scripts/railroad.sh
@@ -1,2 +1,3 @@
-alias uml_model="railroady -M | dot -Tpng > ./doc/models.png"
-alias uml_controller="railroady -C | dot -Tpng > ./doc/controller.png"
+alias uml_model="railroady -M | dot -Tpng > ./doc/models.png && echo 'UML Model successfully generated'"
+alias uml_controller="railroady -C | dot -Tpng > ./doc/controller.png && echo 'UML Controller successfully generated'"
+alias uml_all="railroady -M | dot -Tpng > ./doc/models.png && railroady -C | dot -Tpng > ./doc/controller.png && echo 'UML successfully generated'"
